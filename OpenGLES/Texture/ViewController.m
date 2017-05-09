fa2683876a24108e7188402a77cc9d34b80998f1
Li_guang <liliguang@LLGMBP.local>
2017-05-09 23:03:46 +0800

    修改纹理Demo
    修改纹理Demo

--- OpenGLES/Texture/ViewController.m
+++ OpenGLES/Texture/ViewController.m
@@ -9,8 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (strong, nonatomic) EAGLContext *mContext;
-@property (strong, nonatomic) GLKBaseEffect *mEffect;
+@property (strong, nonatomic) EAGLContext *mContext;            //创建上下文
+@property (strong, nonatomic) GLKBaseEffect *mEffect;           //效果器
 @end
 
 @implementation ViewController
@@ -29,6 +29,7 @@
 - (GLKBaseEffect *)mEffect {
     if (_mEffect == nil) {
         _mEffect = [[GLKBaseEffect alloc]init];
+        //这里面可以设置效果器的一些默认效果
     }
     return _mEffect;
 }
@@ -42,7 +43,7 @@
     view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
     [EAGLContext setCurrentContext:self.mContext];
     glEnable(GL_DEPTH_TEST);
-    
+
     //新的图形
     [self renderNew];
 
@@ -52,6 +53,7 @@
 - (void)renderNew {
     //顶点数据，前三个是顶点坐标， 中间三个是顶点颜色，    最后两个是纹理坐标
     GLfloat attrArr[] = {
+        //  x,     y,    z,     r,    g,    b,    a,       s,    t,
         -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 1.0f,    0.0f, 1.0f,
          0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f,    1.0f, 1.0f,
         -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 1.0f, 1.0f,    0.0f, 0.0f,
@@ -63,11 +65,13 @@
         1, 2, 3,
     };
     
+    //VBO
     GLuint buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(attrArr), attrArr, GL_STATIC_DRAW);
     
+    //IBO
     GLuint index;
     glGenBuffers(1, &index);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
@@ -80,21 +84,22 @@
     glEnableVertexAttribArray(GLKVertexAttribColor);
     glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, (GLfloat *)NULL + 3);
    
-    //纹理属性
+    //纹理属性0
     glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
     glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, (GLfloat *)NULL + 3 + 4);
     
+    //纹理属性1
     glEnableVertexAttribArray(GLKVertexAttribTexCoord1);
     glVertexAttribPointer(GLKVertexAttribTexCoord1, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, (GLfloat *)NULL + 3 + 4);
     
-    //加载纹理
+    //加载纹理0
     NSString* filePath0 = [[NSBundle mainBundle] pathForResource:@"texture0.jpg" ofType:@""];
     NSDictionary* options0 = [NSDictionary dictionaryWithObjectsAndKeys:@(1), GLKTextureLoaderGenerateMipmaps, nil];
     GLKTextureInfo* textureInfo0 = [GLKTextureLoader textureWithContentsOfFile:filePath0 options:options0 error:nil];
     self.mEffect.texture2d0.enabled = GL_TRUE;
     self.mEffect.texture2d0.name = textureInfo0.name;
     
-    //加载纹理
+    //加载纹理1
     NSString* filePath1 = [[NSBundle mainBundle] pathForResource:@"texture1.png" ofType:@""];
     NSDictionary* options1 = [NSDictionary dictionaryWithObjectsAndKeys:@(1), GLKTextureLoaderOriginBottomLeft, nil];
     GLKTextureInfo* textureInfo1 = [GLKTextureLoader textureWithContentsOfFile:filePath1 options:options1 error:nil];
@@ -102,67 +107,6 @@
     self.mEffect.texture2d1.name = textureInfo1.name;
 
 
-}
-
-#pragma mark - 纹理属性单独一个数组
-- (void)renderNew2 {
-    //顶点数据，前三个是顶点坐标， 中间四个是顶点颜色
-    GLfloat attrArr[] = {
-        -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 1.0f,
-         0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f,
-        -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 1.0f, 1.0f,
-         0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 1.0f,
-    };
-    //顶点索引
-    GLuint indices[] = {
-        0, 1, 2,
-        1, 2, 3,
-    };
-    
-    
-    GLfloat texCoord[] = {
-        0.0f,0.0f,
-        1.0f,0.0f,
-        0.5f,1.0f,
-    };
-    
-    
-    GLuint buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(attrArr), attrArr, GL_STATIC_DRAW);
-    
-    GLuint index;
-    glGenBuffers(1, &index);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    
-    
-#warning 这里要思考一个问题,纹理属性单独设置的话,那么应该绑定哪个缓冲区?还是直接使用?
-    GLuint texIBO;
-    glGenBuffers(1, &texIBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, texIBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(texCoord), texCoord, GL_STATIC_DRAW);
-    
-    //坐标
-    glEnableVertexAttribArray(GLKVertexAttribPosition);
-    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, (GLfloat *)NULL);
-    //顶点颜色
-    glEnableVertexAttribArray(GLKVertexAttribColor);
-    glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, (GLfloat *)NULL + 3);
-    
-    //纹理属性
-    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
-    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, (GLfloat *)NULL + 3 + 4);
-    
-    //加载纹理
-    NSString* filePath = [[NSBundle mainBundle] pathForResource:@"texture.jpg" ofType:@""];
-    NSDictionary* options = [NSDictionary dictionaryWithObjectsAndKeys:@(1), GLKTextureLoaderOriginBottomLeft, nil];
-    GLKTextureInfo* textureInfo = [GLKTextureLoader textureWithContentsOfFile:filePath options:options error:nil];
-    self.mEffect.texture2d0.enabled = GL_TRUE;
-    self.mEffect.texture2d0.name = textureInfo.name;
-    
-    
 }
 
 
